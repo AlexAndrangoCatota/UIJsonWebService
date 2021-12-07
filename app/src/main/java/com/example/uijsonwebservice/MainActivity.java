68dd6866d437f273b647c3e2f37ad4e28489ead5
AlexAndrangoCatota <90364913+AlexAndrangoCatota@users.noreply.github.com>
2021-12-07 16:40:53 -0500

    version 2

--- app/src/main/java/com/example/uijsonwebservice/MainActivity.java
+++ app/src/main/java/com/example/uijsonwebservice/MainActivity.java
@@ -5,28 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        initUI();
+        button.setOnClickListener((view) ->  {
+                startActivity(new Intent(MainActivity.this, validarLogin.class));
+        });
+
+    }
+    private void initUI(){
+        button = findViewById(R.id.button);
     }
 
-    public void btEnviar(View view){
-        //Creamos el Intent
-        Intent intent = new Intent(MainActivity.this, validarLogin.class);
-        EditText txtUsuario = (EditText)findViewById(R.id.txtUsuario);
-        EditText txtPass = (EditText)findViewById(R.id.txtPass);
-        // Creamos la información a pasar entre actividades - Pares Key-Value
-        Bundle b = new Bundle();
-        b.putString("usuario", txtUsuario.getText().toString());
-        b.putString("pass", txtPass.getText().toString());
-        // Añadimos la información al intent
-        intent.putExtras(b);
-        // Iniciamos la nueva actividad
-        startActivity(intent);
-        }
+
+//    public void btEnviar(View view){
+//        //Creamos el Intent
+//        Intent intent = new Intent(MainActivity.this, validarLogin.class);
+//        EditText txtUsuario = (EditText)findViewById(R.id.txtUsuario);
+//        EditText txtPass = (EditText)findViewById(R.id.txtPass);
+//        // Creamos la información a pasar entre actividades - Pares Key-Value
+//        Bundle b = new Bundle();
+//        b.putString("usuario", txtUsuario.getText().toString());
+//        b.putString("pass", txtPass.getText().toString());
+//        // Añadimos la información al intent
+//        intent.putExtras(b);
+//        // Iniciamos la nueva actividad
+//        startActivity(intent);
+//    }
+
+
     }
--- app/src/main/java/com/example/uijsonwebservice/validarLogin.java
+++ app/src/main/java/com/example/uijsonwebservice/validarLogin.java
@@ -1,10 +1,17 @@
 package com.example.uijsonwebservice;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.uijsonwebservice.WebService.Asynchtask;
 import com.example.uijsonwebservice.WebService.WebService;
 
@@ -13,30 +20,65 @@ import org.json.JSONException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class validarLogin extends AppCompatActivity implements Asynchtask {
+//public class validarLogin extends AppCompatActivity implements Asynchtask {
+    public class validarLogin extends AppCompatActivity {
+    TextView lblMensaje;
+    RequestQueue requestQueue;
+    private static final String URL = "https://gorest.co.in/public/v1/users?page=1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validar_login);
 
-        Bundle bundle = this.getIntent().getExtras();
 
-        Map<String, String> datos = new HashMap<String, String>();
-        WebService ws= new
-                //WebService("http://uealecpeterson.net/ws/login.php?usr="
-                WebService("https://gorest.co.in/public/v1/users?page=1"
-                + bundle.getString("usuario") + "&pass=" + bundle.getString("pass"),
-                datos, validarLogin.this, validarLogin.this);
-        ws.execute("GET");
+        requestQueue = Volley.newRequestQueue(this);
+
+        initUI();
+        stringRequest();
+//        Bundle bundle = this.getIntent().getExtras();
+//
+//        Map<String, String> datos = new HashMap<String, String>();
+//        WebService ws= new
+//                //WebService("http://uealecpeterson.net/ws/login.php?usr="
+//                WebService("https://gorest.co.in/public/v1/users?page=1"
+//                + bundle.getString("usuario") + "&pass=" + bundle.getString("pass"),
+//                datos, validarLogin.this, validarLogin.this);
+//        ws.execute("GET");
+//
+//    }
+//
+//    @Override
+//    public void processFinish(String result) throws JSONException {
+//        TextView txtSaludo = (TextView)findViewById(R.id.lblMensaje);
+//        //txtSaludo.setText("Hola!, Bienvenido \n " + bundle.getString("usuario")+"\n contraseña \n "+bundle.getString("pass"));
+//        txtSaludo.setText("Respuesta del Web Service"+result);
+//
+//    }
+
 
     }
-
-    @Override
-    public void processFinish(String result) throws JSONException {
-        TextView txtSaludo = (TextView)findViewById(R.id.lblMensaje);
-        //txtSaludo.setText("Hola!, Bienvenido \n " + bundle.getString("usuario")+"\n contraseña \n "+bundle.getString("pass"));
-        txtSaludo.setText("Respuesta del Web Service"+result);
-
+    private void initUI() { lblMensaje = findViewById(R.id.lblMensaje);
     }
+    private void stringRequest(){
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        lblMensaje.setText(response);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        requestQueue.add(request);
+    }
+
 }
